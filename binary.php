db7a650c23f84b8d0cfc2c5b841ac034697a575e
jasonmilli <jasonmllngtn@gmail.com>
2015-07-01 19:20:19 +0000

    binclckcomplete

--- binary.php
+++ binary.php
@@ -1,13 +1,13 @@
 <?php
 require 'gpio.php';
-$pins = array(17,27,5,6,19);
+$pins = array(17,4,27,22,19);
 $leds = array();
 foreach ($pins as $pin) {
     $leds[] = new GPIO($pin);
 }
-for ($i = 0; $i < 100; $i++) {
+for ($i = 0; $i < 50; $i++) {
     foreach ($leds as $j => $led) {
-        if ($i % pow(2, $j) == 0) $led->on();
+        if ($i % pow(2, $j + 1) > pow(2,$j) || $i % pow(2,$j + 1) == 0) $led->on();
         else $led->off();
     }
     sleep(1);
